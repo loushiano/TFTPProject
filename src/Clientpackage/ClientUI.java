90e19156b459fd32c5bf15b0c5332b388f7b09e1
sahajarora1286 <sahajarora1286@gmail.com>
2016-09-26 18:01:50 -0400

    Test Commit Client Ui
    Testing a commit with the client Ui file added.

---
+++ src/Clientpackage/ClientUI.java
@@ -0,0 +1,48 @@
+package Clientpackage;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ClientUI {
+	 public static void main(String[] args) {
+
+	        BufferedReader br = null;
+
+	        try {
+
+	            br = new BufferedReader(new InputStreamReader(System.in));
+
+	            while (true) {
+
+	                System.out.print("Enter command: ");
+	                String input = br.readLine();
+
+	                if (input.equals("rrq")){
+	                	
+	                	System.out.println("Sending a read request.");
+	                    
+	                }
+	                if ("q".equals(input)) {
+	                    System.out.println("Exit!");
+	                    System.exit(0);
+	                }
+
+	                System.out.println("input : " + input);
+	                System.out.println("-----------\n");
+	            }
+
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        } finally {
+	            if (br != null) {
+	                try {
+	                    br.close();
+	                } catch (IOException e) {
+	                    e.printStackTrace();
+	                }
+	            }
+	        }
+
+	    }
+}
