8205b0f27a1d905acae3148cec457ed80e0cc82b
husseinmourad <husseinmourad@CB5107-20.labs.sce.carleton.ca>
2016-09-27 17:51:06 -0400

    Client Change
    last modified client

--- src/Clientpackage/Client.java
+++ src/Clientpackage/Client.java
@@ -20,6 +20,7 @@ public class Client {
 
    DatagramPacket sendPacket, receivePacket;
    DatagramSocket sendReceiveSocket;
+
    
    private static final byte READ = 1, WRITE = 2, INVALID = 5;
 
@@ -58,9 +59,9 @@ public class Client {
 	                     + 1];
 	   
 	   request[0] = 0;
-	   request[1] = 2;
+	   request[1] =1;
 	   
-	   int j = 0;
+	  int j = 0;
 	   //Store bytes of fileName in the request array
 	   for (int i = 2; i < (fileNameBinary.length+2); i++){
 		   
@@ -102,7 +103,7 @@ public class Client {
       //  23 - the destination port number on the destination host.
       try {
          sendPacket = new DatagramPacket(request, request.length,
-                                         InetAddress.getLocalHost(), 23);
+                                         InetAddress.getLocalHost(), 69);//IRAQI: we put 69 because we do not have host now
       } catch (UnknownHostException e) {
          e.printStackTrace();
          System.exit(1);
@@ -131,8 +132,13 @@ public class Client {
 
       // Construct a DatagramPacket for receiving packets up 
       // to 100 bytes long (the length of the byte array).
-
-      byte data[] = new byte[100];
+      while(true){  // we want it to stay looping and scanning for read & write requests 
+    	  
+    	  
+    	  
+    	  
+    	 //===========>>>>> we copeid  this part to the read request below
+      /*byte data[] = new byte[100]; 
       receivePacket = new DatagramPacket(data, data.length);
 
       try {
@@ -156,11 +162,167 @@ public class Client {
       System.out.println(received);
       
       System.out.print("Containing Bytes: ");
-      System.out.println(Arrays.toString(Utility.getBytes(receivePacket.getData(), len)));
+      System.out.println(Arrays.toString(Utility.getBytes(receivePacket.getData(), len)));*/
    
-   }
-   
+      //huss and iraqi
   
+   if (request[1]==READ ){
+	   
+	   byte data[] = new byte[100];
+	      receivePacket = new DatagramPacket(data, data.length);
+
+	      try {
+	         // Block until a datagram is received via sendReceiveSocket.  
+	         sendReceiveSocket.receive(receivePacket);
+	      } catch(IOException e) {
+	         e.printStackTrace();
+	         System.exit(1);
+	      }
+
+	      // Process the received datagram.
+	      System.out.println("Client: Packet received:");
+	      System.out.println("From host: " + receivePacket.getAddress());
+	      System.out.println("Host port: " + receivePacket.getPort());
+	      len = receivePacket.getLength();
+	      System.out.println("Length: " + len);
+	      System.out.print("Containing: ");
+
+	      // Form a String from the byte array.
+	      String received = new String(data,0,len);   
+	      System.out.println(received);
+	      
+	      System.out.print("Containing Bytes: ");
+	      System.out.println(Arrays.toString(Utility.getBytes(receivePacket.getData(), len)));
+		 
+	   byte[] ACK = {0,4,0,1};
+		DatagramPacket sendPacketACK = new DatagramPacket(ACK, ACK.length);
+	   
+		try {
+			sendReceiveSocket.send(sendPacketACK);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		
+		System.out.println( "Cleint sending acknolegment ");
+		System.out.println("To Server: " + receivePacket.getAddress());
+		System.out.println("Destination Server port: " + receivePacket.getPort());
+		len = sendPacketACK.getLength();
+		System.out.println("Length: " + len);
+		System.out.print("Containing: ");
+		System.out.println(new String(sendPacketACK.getData(),0,len));
+		System.out.print("Containing Bytes: ");
+	    System.out.println(Arrays.toString(Utility.getBytes(sendPacketACK.getData(), len)));
+	   
+	   //incrementing the block # in the ACK by calling ali fawaz method
+	   
+   }else if(request[1]==WRITE){
+	   
+	    byte[] sendingData = null;
+		sendingData=new byte[516];
+		sendingData[0] = 0;
+		sendingData[1] = 3;
+		sendingData[2] = 0;
+		sendingData[3] = 1;
+		BufferedInputStream in = null;
+		try {
+			in = new BufferedInputStream(new FileInputStream("C:/Users/Iman/Desktop/workspace/Sysc3303/src/serverPackage/in.dat.txt"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+		        byte[] data1 = new byte[512];
+		        byte[] ACK = new byte[4];
+		        DatagramPacket receivePacketACK = new DatagramPacket(ACK, ACK.length);
+		        int n;
+		        
+		        /* Read the file in 512 byte chunks. */
+		        
+		        try {
+					while ((n = in.read(data1)) != -1) {
+					    /* 
+					     * We just read "n" bytes into array data. 
+					     * Now write them to the output file. 
+					     */
+						System.arraycopy(data1,0,sendingData,4,data1.length);
+						sendPacket = new DatagramPacket(sendingData, sendingData.length,
+								receivePacket.getAddress(), receivePacket.getPort());
+
+						System.out.println( "Server: Sending packet:");
+						System.out.println("To host: " + sendPacket.getAddress());
+						System.out.println("Destination host port: " + sendPacket.getPort());
+						len = sendPacket.getLength();
+						System.out.println("Length: " + len);
+						System.out.print("Containing: ");
+						System.out.println(new String(sendPacket.getData(),0,len));
+						System.out.print("Containing Bytes: ");
+					    System.out.println(Arrays.toString(Utility.getBytes(sendPacket.getData(), len)));
+					    
+					    //send data
+					    try {
+							sendReceiveSocket.send(sendPacket);
+						} catch (IOException e) {
+							e.printStackTrace();
+							System.exit(1);
+						}
+
+						
+						//get acknowledge
+					    try {
+							sendReceiveSocket.receive(receivePacketACK);
+						} catch (IOException e) {
+							e.printStackTrace();
+							System.exit(1);
+						}
+						
+						System.out.println("Server: Packet received:");
+						System.out.println("From host: " + receivePacketACK.getAddress());
+						System.out.println("Host port: " + receivePacketACK.getPort());
+						len = receivePacketACK.getLength();
+						System.out.println("Length: " + len);
+						System.out.print("Containing: " );
+
+						// Form a String from the byte array.
+						String received = new String(data1,0,len);   
+						System.out.println(received + "\n");
+						System.out.print("Containing Bytes: ");
+					    System.out.println(Arrays.toString(Utility.getBytes(receivePacketACK.getData(), len)));
+						
+						
+						
+					    
+					  
+					  
+					}
+					    
+					}
+				 catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		        try {
+					in.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+						
+						
+			// we need to increment using fawazs method		    
+	   
+   
+	   
+
+	   
+					
+   
+   
+   }//end elseif 
+   }//end while
+   
+   }//endof the method 
 
    public static void main(String args[])
    {
