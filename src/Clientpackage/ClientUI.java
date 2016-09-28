cacc4fdc7cf969075e24eeccb7490d9fc1d71c63
sahajarora1286 <sahajarora1286@gmail.com>
2016-09-27 22:13:55 -0400

    Client UI complete.
    Client UI is complete. The file path input has no validations on it yet.
    We will need to discuss it.

--- src/Clientpackage/ClientUI.java
+++ src/Clientpackage/ClientUI.java
@@ -4,14 +4,18 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-import utilities.Contants;
+import utilities.Constants;
 
 public class ClientUI {
-	Client c = new Client();
+	static Client c = new Client();
 	
 	 public static void main(String[] args) {
 
 	        BufferedReader br = null;
+	        
+	        String requestType = "", filePath, vqMode;
+	
+	        String tnMode = Constants.NORMAL;
 
 	        try {
 
@@ -19,25 +23,80 @@ public class ClientUI {
 
 	            while (true) {
 
+	            	System.out.println("Default Mode is " + tnMode);
+	            	System.out.println();
+	            	System.out.println("Type help for a list of available commands.");
+	            	System.out.println();
 	                System.out.print("Enter command: ");
 	                String input = br.readLine();
-
-	                if (input.equals("rrq")){
-	                	
-	                	
-	                	//c.sendAndReceive(requestType, filePath, vqMode, tnMode);
-	                	System.out.println("Sending a read request.");
+	                
+	                if (input.equals("help")){
+	                	System.out.println();
+	                	System.out.println("List of Commands:");
+	                	System.out.println("wrq - Send a write request");
+	                	System.out.println("rrq - Send a read request");
+	                	System.out.println("mode - Display the current mode (Normal/Test)");
+	                	System.out.println("changeMode - Change the mode (Normal/Test)");
+	                	System.out.println("shutdown - Shut down Client");
+	                	System.out.println();
+	                }
+	                
+	                else if (input.equals("mode")) {
+	                	System.out.println("Current mode: " + tnMode);
+	                	System.out.println();
+	                }
+	                
+	                else if (input.equals("changeMode")){
+	                	System.out.print("Set mode (normal/test): ");
+	                    input = br.readLine();
+	                    while (!(input.equals(Constants.NORMAL) || (input.equals(Constants.TEST)))){
+	                		System.out.print("Please enter correct mode (normal or test): ");
+		                	input = br.readLine();
+	                	}
+	                    
+	                    tnMode = input;
+	                    System.out.println("Mode set to " + input);
+	                    System.out.println();
 	                    
 	                }
 	                
+	                else if (input.equals("rrq") || input.equals("wrq")){	                	
+	                	
+	                	if (input.equals("rrq"))	requestType = Constants.READ_REQUEST;
+	                	else if (input.equals("wrq"))	requestType = Constants.WRITE_REQUEST;
+      	
+	                	System.out.print("Enter file path: ");
+	                	input = br.readLine();
+	                	filePath = input;
+	                	System.out.println();
+	                	System.out.print("Enter mode (verbose or quiet): ");
+	                	input = br.readLine();
+	                	
+	                	while (!(input.equals(Constants.VERBOSE) || (input.equals(Constants.QUIET)))){
+	                		System.out.print("Please enter correct mode (verbose or quiet): ");
+		                	input = br.readLine();
+	                	}
+	                	
+	                	vqMode = input;
+	                	System.out.println();
+	                	
+	            
+	                	c.sendAndReceive(requestType, filePath, vqMode, tnMode);
+	                	
+	                    
+	                } 
+	                
 	                //Adding a test commit comment
-	                if ("q".equals(input)) {
-	                    System.out.println("Exit!");
+	                else if ("shutdown".equals(input)) {
+	                    System.out.println("Client shut down.");
 	                    System.exit(0);
 	                }
 
-	                System.out.println("input : " + input);
-	                System.out.println("-----------\n");
+	                else {
+	                	System.out.println("Command not recognized. Please try again.");
+	                }
+	                
+	               
 	            }
 
 	        } catch (IOException e) {
--- src/utilities/Constants.java
+++ src/utilities/Constants.java
@@ -1,15 +1,15 @@
 package utilities;
 
-public class Contants {
+public class Constants {
 	
 	public static final String READ_REQUEST = "READ";
 	public static final String WRITE_REQUEST = "WRITE";	
 	public static final String INVALID_REQUEST = "INVALID";
 	
-	public static final String VERBOSE = "VERBOSE";
-	public static final String QUIET = "QUIET";
-	public static final String TEST = "TEST";
-	public static final String NORMAL = "NORMAL";
+	public static final String VERBOSE = "verbose";
+	public static final String QUIET = "quiet";
+	public static final String TEST = "test";
+	public static final String NORMAL = "normal";
 	
 	public static final String MODE = "octet";
 
